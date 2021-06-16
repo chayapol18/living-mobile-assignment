381fd20589dd34d08cdbd1191d23d89d0ca3a9a4
chayapol <chayapol.s18@gmail.com>
2021-06-17 01:04:22 +0700

    add controller, route and path for menu feature

---
+++ backend-rest/controller/menuController.js
@@ -0,0 +1,85 @@
+const { Catagory, Menu } = require('../models')
+
+exports.getAllMenu = async (req, res, next) => {
+  try {
+    const menu = await Menu.findAll()
+
+    res.status(200).json({ menu })
+
+  } catch (err) {
+    next (err)
+  }
+}
+
+exports.getMenu = async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const menu = await Menu.findOne({ where: { id }})
+
+    res.status(200).json({ menu })
+  } catch (err) {
+    next (err)
+  }
+}
+
+exports.createMenu = async (req, res, next) => {
+  try {
+    const { name, price, catagoryId } = req.body
+
+    const category = await Catagory.findOne({ where: { id: catagoryId }})
+    if (!category) return res.status(400).json({ message: 'This category is not defined'})
+    
+    const menu = await Menu.create({
+      name,
+      price,
+      catagoryId
+    })
+
+    res.status(201).json({ menu })
+
+  } catch (err) {
+    next (err)
+  }
+}
+
+exports.updateMenu = async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const { name, price, catagoryId } = req.body
+
+    const category = await Catagory.findOne({ where: { id: catagoryId }})
+    if (!category) return res.status(400).json({ message: 'This category is not defined'})
+
+    await Menu.update({
+      name,
+      price,
+      catagoryId
+    },
+    {
+      where: { id }
+    }
+    )
+
+    res.status(200).json({ message: 'update menu data complete' })
+
+  } catch (err) {
+    next (err)
+  }
+}
+
+exports.deleteMenu = async (req, res, next) => {
+  try {
+    const { id } = req.params
+
+    const menu = await Menu.findOne({ where: { id } })
+
+    if (!menu) return res.status(400).json({ message: 'menu not found'})
+
+    await menu.destroy();
+
+    res.status(204).json();
+
+  } catch (err) {
+    next (err)
+  }
+}
